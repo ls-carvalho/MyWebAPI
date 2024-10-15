42b502c43ba8596253344c7c0d7b82aa26b30b67
Arthur Santos Almeida <arthur.almeida@itixti.com.br>
2024-10-15 10:16:24 -0300

    Removing Account endpoints: create and delete

--- Services/Interfaces/IAccountService.cs
+++ Services/Interfaces/IAccountService.cs
@@ -6,7 +6,5 @@ public interface IAccountService
 {
     Task<IEnumerable<Account>> GetAllAccountsAsync();
     Task<Account?> GetAccountByIdAsync(int id);
-    Task<Account> CreateAccountAsync(CreateAccountDto account);
     Task<Account> UpdateAccountAsync(UpdateAccountDto account);
-    Task<Account> DeleteAccountAsync(int id);
 }
